7eec21119b4af0773ec9441d0e9543fdaf48d525
rbanowati <restu.banowati@gmail.com>
2013-11-15 08:35:32 +0700

    Update and rename CSV2XML.py to CSVkeXML.py

--- CSVkeXML.py
+++ CSVkeXML.py
@@ -1,19 +1,18 @@
 import csv
 
-csvFile = 'coba2.csv'
-xmlFile = 'coba2.xml'
+csvFile = 'Coba2.csv'
+xmlFile = 'Coba2.xml'
 
 csvData = csv.reader(open(csvFile))
 xmlData = open(xmlFile, 'w')
 xmlData.write('<?xml version="1.0"?>' + "\n")
-# there must be only one top-level tag
 xmlData.write('<csv_data>' + "\n")
 
 rowNum = 0
 for row in csvData:
     if rowNum == 0:
         tags = row
-        # replace spaces w/ underscores in tag names
+        
         for i in range(len(tags)):
             tags[i] = tags[i].replace(' ', '_')
     else: 
